a1b4f69e077ce15473b2c3d7fe37d6710f2397f9
Aayan Ali <aayan3952@gmail.com>
2025-08-28 06:36:17 +0000

    now please ready our app for deployment at GitHub like no key should be

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAqJkpa_JlAroRiusk-K9IUDt1_Li00PPg",
-  authDomain: "stepform-4b234.firebaseapp.com",
-  projectId: "stepform-4b234",
-  storageBucket: "stepform-4b234.appspot.com",
-  messagingSenderId: "739624413935",
-  appId: "1:739624413935:web:20dfc42f2ec534e026091a",
-  measurementId: "G-HZPZTVEN7S"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
